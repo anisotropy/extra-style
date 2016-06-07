1342baec27972653d82f1879e93d70a37f859003
hodoug <j.hodoug@gmail.com>
2016-06-08 04:16:02 +0900

    리팩토링 완료.

--- app/resp-style.js
+++ app/resp-style.js
@@ -1,7 +1,11 @@
 (function($){
 	var _bp = [320, 768, 1024, 1280];
+	var setScrollbarEvent = false;
 
-	$(document).ready(function(){
+	$(window).on('setScrollbarEvent', function(){
+		if(setScrollbarEvent !== false) return;
+		setScrollbarEvent = true;
+
 		var scrollbar = (window.innerWidth != $(window).outerWidth());
 		setInterval(function(){
 			if(scrollbar && window.innerWidth == $(window).outerWidth()){
@@ -41,17 +45,13 @@
 				if($(this).is('.es.es-cell')) $(this).addClass('es-nested');
 				var $target = $(this);
 				var grid = new Grid(arg, $target.children('.es.es-cell').length);
-				/*
-				var curGData = calcGrid(gridData);
-				adjustCell($target, curGData);
-				$(window).on('completeResize', function(){
-					curGData = calcGrid(gridData);
-					adjustCell($target, curGData);
+				grid.adjust($target);
+				$(window).resize(function(){
+					grid.adjust($target);
 				});
 				$(window).on('changeScrollbar', function(){
-					adjustWidth($target, curGData);
+					grid.adjust($target, false);
 				});
-				*/
 			});
 		});
 	}
@@ -59,29 +59,29 @@
 	// style ////
 	function Style(arg){
 		var this_data = {};
-		var this_lbp = [];
+		var this_bp = { bp: undefined, bpi: 0, ww: 0 };
 		var this_style = {};
 		this.css = function(calc){
-			if(calc === undefined || calc) this.calcStyle(this_data, this_lbp, this_style);
+			if(calc === undefined || calc) this.calcStyle(this_data, this_bp, this_style);
 			return this_style;
 		}
 		//initialize ////
 		if(arg){
-			this.convToObj(arg, this_data, this_lbp);
-			this.validate(this_data, this_lbp);
-			this.calcEqual(this_data);
-			this.calcMiddle(this_data, this_lbp);
+			this.convToObj(arg, this_data, this_bp);
+			this.postConv(this_data, this_bp);
 		}
 	}
-	Style.prototype.convToObj = function(arg, data, lbp){
+	Style.prototype.convToObj = function(arg, data, bp){
 		for(var prop in arg){
 			if(prop !== 'breakpoint'){
 				data[prop] = { series: [], max: false, unit: 'px', func: 'linear'};
 				this.convToObjEach(arg[prop], data[prop]);
 			} else {
-				this.convToObjEach(arg[prop], lbp);
+				bp.bp = [];
+				this.convToObjEach(arg[prop], bp.bp);
 			}
 		}
+		if(bp.bp === undefined) bp.bp = _bp;
 	}
 	Style.prototype.convToObjEach = function(propOfArg, propOfData){
 		var array = propOfArg.split(' ');
@@ -103,93 +103,90 @@
 			}
 		}
 	}
-	Style.prototype.validate = function(data, lbp){
-		var bpLength;
-		if(lbp.length == 0) bpLength = _bp.length; else bpLength =  lbp.length;
+	Style.prototype.postConv = function(data, bp){
 		for(var prop in data){
-			if($.type(data[prop]) === 'object'){
-				if(data[prop].func !== 'saw' && data[prop].series.length < bpLength){
-					for(var i = data[prop].series.length, len = bpLength; i < len; i++){
-						data[prop].series[i] = data[prop].series[i-1];
-					}
+			this.validateEach(data[prop], bp.bp.length);
+			this.calcMiddleEach(data[prop], bp.bp);
+			this.calcEqualEach(data[prop]);
+		}
+	}
+	Style.prototype.validateEach = function(propOfData, bpLength){
+		if($.type(propOfData) === 'object'){
+			if(propOfData.func !== 'saw' && propOfData.series.length < bpLength){
+				for(var i = propOfData.series.length, len = bpLength; i < len; i++){
+					propOfData.series[i] = propOfData.series[i-1];
 				}
-				else if(data[prop].func === 'saw' && data[prop].series.length <bpLength*2){
-					for(var i = data[prop].series.length, len = bpLength*2; i < len; i++){
-						data[prop].series[i] = data[prop].series[i-1];
-					}
+			}
+			else if(propOfData.func === 'saw' && propOfData.series.length <bpLength*2){
+				for(var i = propOfData.series.length, len = bpLength*2; i < len; i++){
+					propOfData.series[i] = propOfData.series[i-1];
 				}
-			} else if($.type(data[prop]) === 'array'){
-				if(data[prop].length < bpLength){
-					for(var i = data[prop].length, len = bpLength; i < len; i++){
-						data[prop][i] = data[prop][i-1];
-					}
+			}
+		} else if($.type(propOfData) === 'array'){
+			if(propOfData.length < bpLength){
+				for(var i = propOfData.length, len = bpLength; i < len; i++){
+					propOfData[i] = propOfData[i-1];
 				}
 			}
 		}
 	}
-	Style.prototype.calcMiddle = function(data, lbp){
-		var bp; if(lbp.length == 0) bp = _bp; else bp = lbp;
-		for(var prop in data){
-			var series;
-			if($.type(data[prop]) === 'object') series = data[prop].series;
-			else if($.type(data[prop]) === 'array') series = data[prop];
-			var pni = 0, nni = 0;
-			if(series[0] === '-'){ console.error('Wrong usage of "-"'); return; }
-			for(var i = 0, len = series.length; i < len-1; i++){
-				if($.isNumeric(series[i])) pni = i;
-				if(series[i] === '-'){
-					if(nni === 0 || nni < i){
-						for(var j = i+1; j < len; j++){
-							if($.isNumeric(series[j])){ nni = j; break; }
-						}
-						if(nni === 0 || nni < i) {
-							console.error('Wrong usage of "-"'); return;
-						}
+	Style.prototype.calcMiddleEach = function(propOfData, bp){
+		var series;
+		if($.type(propOfData) === 'object') series = propOfData.series;
+		else if($.type(propOfData) === 'array') series = propOfData;
+		var pni = 0, nni = 0;
+		if(series[0] === '-'){ console.error('Wrong usage of "-"'); return; }
+		for(var i = 0, len = series.length; i < len-1; i++){
+			if($.isNumeric(series[i])) pni = i;
+			if(series[i] === '-'){
+				if(nni === 0 || nni < i){
+					for(var j = i+1; j < len; j++){
+						if($.isNumeric(series[j])){ nni = j; break; }
 					}
-					series[i] = (series[nni] - series[pni]) * (bp[i] - bp[pni]) / (bp[nni] - bp[pni]) + series[pni];
+					if(nni === 0 || nni < i) {
+						console.error('Wrong usage of "-"'); return;
+					}
 				}
+				series[i] = (series[nni] - series[pni]) * (bp[i] - bp[pni]) / (bp[nni] - bp[pni]) + series[pni];
 			}
 		}
-	}
-	Style.prototype.calcEqual = function(data){
-		for(var prop in data){
-			var series;
-			if($.type(data[prop]) === 'object') series = data[prop].series;
-			if(series[0] === '='){ console.error('Wrong usage of "="'); return; }
-			for(var i = 1, len = series.length; i < len; i++){
-				if(series[i] === '=') series[i] = series[i-1];
-			}
-		}
-	}
-	Style.prototype.calcStyle = function(data, lbp, style){
-		var sbp = {};
-		sbp.ww = window.innerWidth;
-		if(lbp.length == 0) sbp.bp = _bp; else sbp.bp = lbp;
-		if(sbp.ww < sbp.bp[0]) sbp.bpi = -1;
-		else if(sbp.ww >=  sbp.bp[sbp.bp.length-1]) sbp.bpi = -2;
-		else { for(var i = 0; i < sbp.bp.length-1; i++){
-			if(sbp.bp[i] <= sbp.ww && sbp.ww < sbp.bp[i+1]){ sbp.bpi = i; break; }
-		}}
 
+	}
+	Style.prototype.calcEqualEach = function(propOfData){
+		var series;
+		if($.type(propOfData) === 'object') series = propOfData.series;
+		else if($.type(propOfData) === 'array') series = propOfData;
+		if(series[0] === '='){ console.error('Wrong usage of "="'); return; }
+		for(var i = 1, len = series.length; i < len; i++){
+			if(series[i] === '=') series[i] = series[i-1];
+		}
+	}
+	Style.prototype.calcStyle = function(data, bp, style){
+		this.findBp(bp);
 		for(var prop in data){
 			if(!this.isNotNumProp(prop)){
-				style[prop] = this.calc(data[prop].series, data[prop].max, data[prop].func, sbp) + data[prop].unit;
+				style[prop] = this.calc(data[prop], bp) + data[prop].unit;
 			} else {
-				style[prop] = this.pick(data[prop].series, sbp);
+				style[prop] = this.pick(data[prop].series, bp);
 			}
 		}
 	}
-	Style.prototype.pick = function(series, sbp){
-		if(sbp.bpi >= 0) return series[sbp.bpi];
-		else if(sbp.bpi == -1) return series[0];
-		else if(sbp.bpi == -2) return series[sbp.bp.length-1];
+	Style.prototype.findBp = function(bp){
+		bp.ww = window.innerWidth;
+		if(bp.ww < bp.bp[0]) bp.bpi = -1;
+		else if(bp.ww >=  bp.bp[bp.bp.length-1]) bp.bpi = -2;
+		else { for(var i = 0; i < bp.bp.length-1; i++){
+			if(bp.bp[i] <= bp.ww && bp.ww < bp.bp[i+1]){ bp.bpi = i; break; }
+		}}
 	}
-	Style.prototype.calc = function(series, max, func, sbp){
-		var bp = sbp.bp;
-		var bpi = sbp.bpi;
-		var len = sbp.bp.length;
-		var ww = sbp.ww;
-
+	Style.prototype.pick = function(series, bp){
+		if(bp.bpi >= 0) return series[bp.bpi];
+		else if(bp.bpi == -1) return series[0];
+		else if(bp.bpi == -2) return series[bp.bp.length-1];
+	}
+	Style.prototype.calc = function(propOfData, this_bp){
+		var series = propOfData.series, max = propOfData.max, func = propOfData.func;
+		var bp = this_bp.bp, bpi = this_bp.bpi, len = this_bp.bp.length, ww = this_bp.ww;
 		var i;
 		if(bpi >= 0){
 			i = bpi;
@@ -237,20 +234,32 @@
 	function Grid(arg, numCell){
 		var this_data = {
 			columns: [], ratio: [],
-			gutter: {series: [], max: false, func: 'linear'},
+			gutter: {series: [], max: false, func: 'linear'}, //gutter의 unit은 px.
 			cells: new Array(numCell)
 		};
-		var this_grid = {};
-		var this_lbp = [];
+		var this_bp = { bp: undefined, bpi: 0, ww: 0 };
+		var this_current = { columns: 0, ratio: 0, gutter: 0 , cells: new Array(numCell) };
+		this.adjust = function($target, cell){
+			if(cell === undefined || cell){
+				this.calcCurrent(this_data, this_bp, this_current);
+				this.adjustCell(this_current, $target);
+			} else {
+				this.adjustCellWidth(this_current, $target);
+			}
+		}
 		//initialize ////
 		if(arg){
-			this.convToObj(arg, this_data, this_lbp);
-			this.postConv(this_data, this_lbp);
+			this.convToObj(arg, this_data, this_bp);
+			this.postConv(this_data, this_bp);
 		}
 	}
 	Grid.prototype = new Style();
-	Grid.prototype.convToObj = function(arg, data, lbp){
-		if(arg.breakpoint) this.convToObjEach(arg.breakpoint, lbp);
+	Grid.prototype.convToObj = function(arg, data, bp){
+		if(arg.breakpoint){
+			bp.bp = []; this.convToObjEach(arg.breakpoint, bp.bp);
+		} else {
+			bp.bp = _bp;
+		}
 		for(var prop in arg){
 			if(data[prop]) this.convToObjEach(arg[prop], data[prop]);
 			else if(prop.match(/^cell/)){
@@ -258,50 +267,58 @@
 				data.cells[index] = [];
 				this.convToObjEach(arg[prop], data.cells[index]);
 			}
-
 		}
 	}
-	Grid.prototype.postConv = function(data, lbp){
-		/*
-		this.validate(data, lbp);
-		this.calcEqual(data);
-		this.calcMiddle(data, lbp);
-		*/
+	Grid.prototype.postConv = function(data, bp){
+		for(var prop in data){
+			if(prop !== 'cells'){
+				this.validateEach(data[prop], bp.bp.length);
+				this.calcMiddleEach(data[prop], bp.bp);
+				this.calcEqualEach(data[prop]);
+			} else {
+				for(var i = 0, len = data[prop].length; i < len; i++){
+					if(data[prop][i]){
+						this.validateEach(data[prop][i], bp.bp.length);
+						this.calcMiddleEach(data[prop][i], bp);
+						this.calcEqualEach(data[prop][i]);
+					}
+				}
+			}
+		}
 	}
-	Grid.prototype.calcGrid = function(data, lbp, grid){
-		var cur = {}, idx;
-		if(_bpi >= 0) idx = _bpi;
-		else if(_bpi == -1) idx = 0;
-		else if(_bpi == -2) idx = _bp.length-1;
-		cur.columns = data.columns[idx];
-		cur.ratio = data.ratio[idx];
-		cur.gutter = calc(data.gutter.series, data.gutter.max, data.gutter.func);
-		cur.cells = [];
+	Grid.prototype.calcCurrent = function(data, this_bp, current){
+		this.findBp(this_bp);
+		current.gutter = this.calc(data.gutter, this_bp);
+		var bpi;
+		if(this_bp.bpi >= 0) bpi = this_bp.bpi;
+		else if(this_bp.bpi === -2) bpi = this_bp.bp.length-1;
+		else if(this_bp.bpi === -1) bpi = 0;
+		current.columns = data.columns[bpi];
+		current.ratio = data.ratio[bpi];
 		for(var i = 0, len = data.cells.length; i < len; i++){
-			 if(data.cells[i]) cur.cells[i] = data.cells[i][idx];
-			 else cur.cells[i] = 1;
+			 if(data.cells[i]) current.cells[i] = data.cells[i][bpi];
+			 else current.cells[i] = 1;
 		}
-		return cur;
 	}
-	function adjustCell($target, curGData){
+	Grid.prototype.adjustCell = function(current, $target){
 		var preW = $target.outerWidth();
-		var gut = curGData.gutter;
-		var cols = curGData.columns;
-		var cells = curGData.cells;
+		var gut = current.gutter;
+		var cols = current.columns;
+		var cells = current.cells;
 		var $cells = $target.children('.es.es-cell');
 		var length = $cells.length;
-		var cw = (getDim($target).width - gut*(cols-1))/cols;
+		var cw = (this.getDim($target).width - gut*(cols-1))/cols;
 		var maxCh = 0;
 		$cells.css({'margin-right': 0, 'margin-top': 0});
-		if(curGData.ratio !== 'auto') $cells.css({width: 0, height: 0});
+		if(current.ratio !== 'auto') $cells.css({width: 0, height: 0});
 		else $cells.css({width: 0, height: 0});
 		$cells.each(function(index){
 			$(this).outerWidth(cw*cells[index] + gut*(cells[index]-1));
 			if((index+1) % cols !== 0 && index < length-1) $(this).css({'margin-right': gut});
 			if(index >= cols) $(this).css({'margin-top': gut});
-			if(curGData.ratio === 'auto'){
+			if(current.ratio === 'auto'){
 				$(this).css({height: ''});
-				if(maxCh < getDim($(this)).height) maxCh = getDim($(this)).height;
+				if(maxCh < this.getDim($(this)).height) maxCh = this.getDim($(this)).height;
 				if(cols !== 1 && (index+1) % cols === 0){
 					for(var i = index - cols + 1; i <= index; i++) $cells.eq(i).outerHeight(maxCh);
 					maxCh = 0;
@@ -309,44 +326,27 @@
 					for(var i = Math.floor((index+1)/cols) * cols; i <= index; i++) $cells.eq(i).outerHeight(maxCh);
 				}
 			} else {
-				var height = cw * curGData.ratio;
+				var height = cw * current.ratio;
 				if($target.is('.es.es-nested')) height -= (gut - gut/Math.ceil(length/cols));
 				$(this).outerHeight(height);
 			}
 		});
 		if(preW != $target.outerWidth()){
-			cw = (getDim($target).width - gut*(cols-1))/cols;
+			cw = (this.getDim($target).width - gut*(cols-1))/cols;
 			$cells.each(function(index){ $(this).outerWidth(cw*cells[index] + gut*(cells[index]-1)); });
 		}
 	}
-	function adjustWidth($target, curGData){
-		var gut = curGData.gutter;
-		var cols = curGData.columns;
-		var cells = curGData.cells;
+	Grid.prototype.adjustCellWidth = function(current, $target){
+		var gut = current.gutter;
+		var cols = current.columns;
+		var cells = current.cells;
 		var $cells = $target.children('.es.es-cell');
-		var cw = (getDim($target).width - gut*(cols-1))/cols;
+		var cw = (this.getDim($target).width - gut*(cols-1))/cols;
 		$cells.each(function(index){
 			$(this).outerWidth(cw*cells[index] + gut*(cells[index]-1));
 		});
 	}
-	/*
-	function calcGrid(data){
-		var cur = {}, idx;
-		if(_bpi >= 0) idx = _bpi;
-		else if(_bpi == -1) idx = 0;
-		else if(_bpi == -2) idx = _bp.length-1;
-		cur.columns = data.columns[idx];
-		cur.ratio = data.ratio[idx];
-		cur.gutter = calc(data.gutter.series, data.gutter.max, data.gutter.func);
-		cur.cells = [];
-		for(var i = 0, len = data.cells.length; i < len; i++){
-			 if(data.cells[i]) cur.cells[i] = data.cells[i][idx];
-			 else cur.cells[i] = 1;
-		}
-		return cur;
-	}
-	*/
-	function getDim($obj){
+	Grid.prototype.getDim = function($obj){
 		return $obj[0].getBoundingClientRect();
 	}
 })(jQuery);
--- test/style-test-1.js
+++ test/style-test-1.js
@@ -1,20 +1,23 @@
 (function($){
 	$.respStyle('320 500 700 1000');
+	/*
 	$('.first-section').respStyle({
 		'breakpoint': '300 400 600 800',
-		//'margin': '0 40 0 40 0 40 saw'
+		'margin': '0 40 0 40 = = saw',
 		'font-size': '10 - - 60'
 		//'float': 'left right none left'
 	});
-
+	*/
 	$('.grid-container').respGrid({
 		'breakpoint': '300 400 600 800',
-		columns: '1 3 3 4',
-		ratio: '0.5 1 2 2',
+		columns: '1 2 3 4',
+		ratio: '0.2 0.3 2 1',
 		//ratio: 'auto',
-		gutter: '10 15 20 30',
-		cell3: '1 1 1 2'
+		gutter: '10 - - 30',
+		cell3: '1 = 2 1',
+		cell4: '1 2 1 4'
 	});
 
+	$(window).trigger('setScrollbarEvent');
 
 })(jQuery);
